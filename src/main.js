c28970d678915c3f2f83c1e5f137af3ed01ee5ac
ctn-osmn94 <cetinosman676@gmail.com>
2022-06-30 22:31:26 +0300

    all categories added

--- src/main.js
+++ src/main.js
@@ -22,7 +22,18 @@ let userProducts = document.getElementById("user-products")
                 if(userCategori == "Yapı Market & Tamirat > Tamir, Tadilat Gereçleri") {
                     getTamirTadilat(products)
                 }
-                
+                if(userCategori == "Ev, Dekorasyon, Bahçe > Mobilya") {
+                    getMobilya(products)
+                }
+                if(userCategori == "Kozmetik & Kişisel Bakım > Sağlık, Medikal") {
+                    getMedical(products)
+                }
+                if(userCategori == "Bilgisayar, Tablet > Dizüstü Bilgisayar (Laptop)") {
+                    getLaptop(products)
+                }
+                if(userCategori == "Beyaz Eşya & Küçük Ev Aletleri > Isıtma, Soğutma Sistemi") {
+                    getStuff(products)
+                }
             })
         });
         
@@ -65,5 +76,78 @@ let userProducts = document.getElementById("user-products")
                      
         }
     }
+
+    function getMobilya(products) {
+        userProducts.innerHTML = ""
+        for (let i = 0; i < products["Ev, Dekorasyon, Bahçe > Mobilya"].length; i++) {
+            const productImage = document.createElement("img")
+            const productPrice = document.createElement("li")
+            const productName = document.createElement("li")
+            const contentProduct = document.createElement("ul")
+            productImage.src = products["Ev, Dekorasyon, Bahçe > Mobilya"][i].image
+            productName.innerHTML = products["Ev, Dekorasyon, Bahçe > Mobilya"][i].name
+            productPrice.innerHTML = products["Ev, Dekorasyon, Bahçe > Mobilya"][i].priceText
+            contentProduct.appendChild(productImage)
+            contentProduct.appendChild(productName)
+            contentProduct.appendChild(productPrice)
+            userProducts.append(contentProduct)
+                     
+        }
+    }
+
+    function getMedical(products) {
+        userProducts.innerHTML = ""
+        for (let i = 0; i < products["Kozmetik & Kişisel Bakım > Sağlık, Medikal"].length; i++) {
+            const productImage = document.createElement("img")
+            const productPrice = document.createElement("li")
+            const productName = document.createElement("li")
+            const contentProduct = document.createElement("ul")
+            productImage.src = products["Kozmetik & Kişisel Bakım > Sağlık, Medikal"][i].image
+            productName.innerHTML = products["Kozmetik & Kişisel Bakım > Sağlık, Medikal"][i].name
+            productPrice.innerHTML = products["Kozmetik & Kişisel Bakım > Sağlık, Medikal"][i].priceText
+            contentProduct.appendChild(productImage)
+            contentProduct.appendChild(productName)
+            contentProduct.appendChild(productPrice)
+            userProducts.append(contentProduct)
+                     
+        }
+    }
+
+    function getLaptop(products) {
+        userProducts.innerHTML = ""
+        for (let i = 0; i < products["Bilgisayar, Tablet > Dizüstü Bilgisayar (Laptop)"].length; i++) {
+            const productImage = document.createElement("img")
+            const productPrice = document.createElement("li")
+            const productName = document.createElement("li")
+            const contentProduct = document.createElement("ul")
+            productImage.src = products["Bilgisayar, Tablet > Dizüstü Bilgisayar (Laptop)"][i].image
+            productName.innerHTML = products["Bilgisayar, Tablet > Dizüstü Bilgisayar (Laptop)"][i].name
+            productPrice.innerHTML = products["Bilgisayar, Tablet > Dizüstü Bilgisayar (Laptop)"][i].priceText
+            contentProduct.appendChild(productImage)
+            contentProduct.appendChild(productName)
+            contentProduct.appendChild(productPrice)
+            userProducts.append(contentProduct)
+                     
+        }
+    }
+
+    function getStuff(products) {
+        userProducts.innerHTML = ""
+        for (let i = 0; i < products["Beyaz Eşya & Küçük Ev Aletleri > Isıtma, Soğutma Sistemi"].length; i++) {
+            const productImage = document.createElement("img")
+            const productPrice = document.createElement("li")
+            const productName = document.createElement("li")
+            const contentProduct = document.createElement("ul")
+            productImage.src = products["Beyaz Eşya & Küçük Ev Aletleri > Isıtma, Soğutma Sistemi"][i].image
+            productName.innerHTML = products["Beyaz Eşya & Küçük Ev Aletleri > Isıtma, Soğutma Sistemi"][i].name
+            productPrice.innerHTML = products["Beyaz Eşya & Küçük Ev Aletleri > Isıtma, Soğutma Sistemi"][i].priceText
+            contentProduct.appendChild(productImage)
+            contentProduct.appendChild(productName)
+            contentProduct.appendChild(productPrice)
+            userProducts.append(contentProduct)
+                     
+        }
+    }
+
 
 
